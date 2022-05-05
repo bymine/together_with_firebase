f89886cb6d8729b5ed5f6c85e5db07dea102f958
bymine <frfr0723@gmail.com>
2022-05-05 16:28:11 +0900

    Setting View UI test

--- lib/src/pages/views/setting_view.dart
+++ lib/src/pages/views/setting_view.dart
@@ -5,6 +5,58 @@ class SettingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                radius: 30,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Text("frfr0723@gmail.com")
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text("내 활동"),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            color: Colors.grey.withOpacity(0.5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text("2"),
+                    Text("프로젝트"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text("10"),
+                    Text("스케줄"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text("2"),
+                    Text("파일"),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
